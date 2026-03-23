f0aab031d4b17f2d93ccd7203c49dcd80cc949b5
Mini-Electro <scott.white0703@gmail.com>
2026-03-23 08:02:30 +0000

    Add files via upload

---
+++ CV2Intro.py
@@ -0,0 +1,17 @@
+import cv2
+
+
+image = cv2.imread("C://Users//ADMIN//Downloads//Intro_to_Computer_Vision___OpenCV_asset-e753//example.jpg")
+
+
+cv2.namedWindow("Loaded Image", cv2.WINDOW_NORMAL)
+
+cv2.resizeWindow("Loaded Image", 800,500)
+
+cv2.imshow("Loaded Image", image)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+print(f"Image Dimensions: {image.shape}") # Height, Width, Channels
